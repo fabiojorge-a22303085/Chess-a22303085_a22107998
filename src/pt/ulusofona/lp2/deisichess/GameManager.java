1316ee2f8af6a1491eec3b20c5f8a8cca6412d4c
Fábio <fabiomljorge@gmail.com>
2023-10-23 21:30:30 +0100

    update

---
+++ src/pt/ulusofona/lp2/deisichess/GameManager.java
@@ -0,0 +1,46 @@
+package pt.ulusofona.lp2.deisichess;
+
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class GameManager {
+
+
+    int getBoardSize() {
+        return 0;
+    }
+
+    boolean move(int x0, int y0, int x1, int y1) {
+        return false;
+    }
+
+    String[] getSquareInfo(int x, int y) {
+        return new String[0];
+    }
+
+    String[] getPieceInfo(int ID) {
+        return new String[0];
+    }
+
+    String getPieceInfoAsString(int ID) {
+        return null;
+    }
+
+    int getCurrentTeamID() {
+        return 0;
+    }
+
+    boolean gameOver() {
+        return false;
+    }
+
+
+     ArrayList<String> getGameResults() {
+        return null;
+    }
+
+     JPanel getAuthorsPanel() {
+        return null;
+     }
+
+}
