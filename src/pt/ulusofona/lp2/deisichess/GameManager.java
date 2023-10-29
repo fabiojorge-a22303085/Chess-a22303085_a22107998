1e8fdcc1bea46272c01bb446998e2072994dfbb8
Goncalo Costa <goncalo.costa2911@gmail.com>
2023-10-29 19:22:28 +0000

    getSquareInfo(correçao null na posição valida)

--- src/pt/ulusofona/lp2/deisichess/GameManager.java
+++ src/pt/ulusofona/lp2/deisichess/GameManager.java
@@ -99,38 +99,6 @@ public class GameManager {
             return false;
         }
 
-
-
-
-        /*if (lines.size() == 12) {
-            boardSize = 4;
-            pecas.add(new Piece(1, 0, 0 , "Chefe", 1, 0));
-            pecas.add(new Piece(2, 0, 0 , "Selvagem", 3, 0));
-            pecas.add(new Piece(3, 0, 0 , "Grande Artista", 2, 1));
-            pecas.add(new Piece(4, 0, 1 , "O Maior", 2, 3));
-            pecas.add(new Piece(5, 0, 1 , "Amigo", 1, 3));
-            pecas.add(new Piece(6, 0, 1 , "Beberolas", 1, 2));
-
-            return true;
-
-        } else if (lines.size() == 20) {
-            boardSize = 8;
-            pecas.add(new Piece(1, 0, 0 , "Chefe", 1, 0));
-            pecas.add(new Piece(2, 0, 0 , "Selvagem", 3, 0));
-            pecas.add(new Piece(3, 0, 0 , "Grande Artista", 2, 1));
-            pecas.add(new Piece(4, 0, 0 , "Amante de Prata", 4, 1));
-            pecas.add(new Piece(5, 0, 0 , "Artolas", 6, 1));
-            pecas.add(new Piece(6, 0, 1 , "Maior", 4, 5));
-            pecas.add(new Piece(7, 0, 1 , "Amigo", 2, 6));
-            pecas.add(new Piece(8, 0, 1 , "Beberolas", 6, 6));
-            pecas.add(new Piece(9, 0, 1 , "Esperto", 2, 7));
-            pecas.add(new Piece(10, 0, 1 , "Barulhento", 4, 7));
-
-            return true;
-
-        } else {
-            return false;
-        } */
     }
 
 
@@ -143,7 +111,7 @@ public class GameManager {
 
         String[] squareInfo = new String[5];
         boolean temPeca = false;
-        if (x > boardSize || x < 0 || y > boardSize || y < 0) {
+        if (x >= boardSize || x < 0 || y >= boardSize || y < 0) {
             return null;
         }
         for(Piece peca : pecas) {
@@ -160,7 +128,7 @@ public class GameManager {
         if(temPeca) {
             return squareInfo;
         } else {
-            return null;
+            return squareInfo;
         }
 
     }
